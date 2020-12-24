861ab4f9efc9e77a16cf3487e2d8a98aeeb90d56
HeeyeonJeong <junghy14971@gmail.com>
2020-12-24 14:58:17 +0900

    Feat: onToggle, onRemove/TodoItem

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled, { css } from "styled-components";
 import { MdDone, MdDelete } from "react-icons/md";
+import { useTodoDispatch } from "../TodoContext";
 
 const CheckCircle = styled.div`
   width: 32px;
@@ -40,7 +41,7 @@ const Remove = styled.div`
   color: #dee2e6;
   font-size: 24px;
   cursor: pointer;
-  &::hover {
+  &:hover {
     color: #ff6b6b;
   }
 `;
@@ -57,15 +58,29 @@ const TodoItemBlock = styled.div`
 `;
 
 function TodoItem({ id, done, text }) {
+  const dispatch = useTodoDispatch();
+  const onToggle = () =>
+    dispatch({
+      type: "TOGGLE",
+      id,
+    });
+  const onRemove = () =>
+    dispatch({
+      type: "REMOVE",
+      id,
+    });
+
   return (
     <TodoItemBlock>
-      <CheckCircle done={done}>{done && <MdDone />}</CheckCircle>
+      <CheckCircle onClick={onToggle} done={done}>
+        {done && <MdDone />}
+      </CheckCircle>
       <Text done={done}>{text}</Text>
-      <Remove>
+      <Remove onClick={onRemove}>
         <MdDelete />
       </Remove>
     </TodoItemBlock>
   );
 }
 
-export default TodoItem;
+export default React.memo(TodoItem);
